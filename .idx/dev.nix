6034e885e1e14faf5fe4cd11b87f67da36e1359e
DONATIONPLLS <127539936+DONATIONPLLS@users.noreply.github.com>
2026-04-26 22:16:12 +0100

    Update dev.nix to configure previews and workspace

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,9 +1,40 @@
 { pkgs, ... }: {
   channel = "stable-23.11";
+
   packages = [
     pkgs.nodejs_20
-    pkgs.pnpm
-    pkgs.android-sdk
     pkgs.corepack_21
   ];
+
+  idx = {
+    extensions = [];
+
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = [
+            "pnpm"
+            "run"
+            "dev"
+            "--port"
+            "$PORT"
+            "--host"
+            "0.0.0.0"
+          ];
+          manager = "web";
+        };
+      };
+    };
+
+    workspace = {
+      onCreate = {
+        install = {
+          command = "pnpm install";
+          description = "Installing dependencies";
+        };
+      };
+      onStart = {};
+    };
+  };
 }
