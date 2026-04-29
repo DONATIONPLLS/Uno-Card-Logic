7236d7889e767101db0a1e0770e4982c4bcdf456
DONATIONPLLS <127539936+DONATIONPLLS@users.noreply.github.com>
2026-04-29 23:46:08 +0100

    Update MainActivity.java

--- android/app/src/main/java/com/unobuddy/app/MainActivity.java
+++ android/app/src/main/java/com/unobuddy/app/MainActivity.java
@@ -1,32 +1,34 @@
 package com.unobuddy.app;
 
-import com.getcapacitor.BridgeActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        makeImmersive();
+    }
+
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
-            hideSystemBars();
+            makeImmersive();
         }
     }
 
-    private void hideSystemBars() {
-        Window window = getWindow();
-        window.getDecorView().setSystemUiVisibility(
+    private void makeImmersive() {
+        getWindow().getDecorView().setSystemUiVisibility(
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
-        WindowManager.LayoutParams a = window.getAttributes();
-        a.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-        window.setAttributes(a);
     }
 }
