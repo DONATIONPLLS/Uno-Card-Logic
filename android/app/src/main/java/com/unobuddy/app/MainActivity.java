26a129ce2bed1394669f50389d63a25356d82bfd
DONATIONPLLS <127539936+DONATIONPLLS@users.noreply.github.com>
2026-04-30 18:35:56 +0100

    Update MainActivity.java

--- android/app/src/main/java/com/unobuddy/app/MainActivity.java
+++ android/app/src/main/java/com/unobuddy/app/MainActivity.java
@@ -16,14 +16,23 @@ public class MainActivity extends BridgeActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        // Make the system bars backgrounds transparent immediately
         Window window = getWindow();
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        
+        // 1. Tell Android we are handling the background ourselves
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        
+        // 2. Make them transparent
         window.setStatusBarColor(Color.TRANSPARENT);
         window.setNavigationBarColor(Color.TRANSPARENT);
 
-        // Tell the window to draw edge-to-edge
+        // 3. KILL THE TINT (The "Subway Surfers" fix)
+        // This stops Android from forcing that semi-transparent grey/black box
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.setStatusBarContrastEnforced(false);
+            window.setNavigationBarContrastEnforced(false);
+        }
+
+        // 4. Set Edge-to-Edge
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.setDecorFitsSystemWindows(false);
         }
@@ -41,13 +50,10 @@ public class MainActivity extends BridgeActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             WindowInsetsController controller = getWindow().getInsetsController();
             if (controller != null) {
-                // Hide bars
                 controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
-                // Make them transparent/transient when swiped
                 controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
             }
         } else {
-            // Older Android versions
             getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
